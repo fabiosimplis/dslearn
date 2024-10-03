acd596345618a76d7feb07baa0f21fe2835805af
fabiosimplis <fabio.simplis@gmail.com>
2024-10-03 18:50:05 -0300

    ADD Course

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,3 +13,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML5', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9IUMiBUfucWPl5p1p6eleNS0j4rsFPh8yWw&s', 'https://upload.wikimedia.org/wikipedia/commons/8/8a/HTML5.png');
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp CSS3', 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg', 'https://cdn.icon-icons.com/icons2/2389/PNG/512/css_shiled_logo_icon_145359.png');
+
